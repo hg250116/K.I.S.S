76cada59a789a819b272d95570f6ccfe846f48f6
hg250116 <hg250116@ncr.com>
2020-07-29 16:21:04 +0300

    upload solution

---
+++ BarcodeTextScanner/BarcodeTextScanner/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace BarcodeTextScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BrowseBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 373);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(561, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Scan Barcode Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 430);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(560, 64);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(447, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // BrowseBtn
+            // 
+            this.BrowseBtn.Location = new System.Drawing.Point(469, 16);
+            this.BrowseBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BrowseBtn.Name = "BrowseBtn";
+            this.BrowseBtn.Size = new System.Drawing.Size(100, 28);
+            this.BrowseBtn.TabIndex = 4;
+            this.BrowseBtn.Text = "Browse";
+            this.BrowseBtn.UseVisualStyleBackColor = true;
+            this.BrowseBtn.Click += new System.EventHandler(this.BrowseBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 49);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(552, 318);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 508);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BrowseBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Barcode Text Scanner";
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BrowseBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ BarcodeTextScanner/BarcodeTextScanner/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+using System.Drawing.Imaging;
+using System.IO;
+using static Emgu.CV.OCR.Tesseract;
+
+namespace BarcodeTextScanner
+{
+    public partial class Form1 : Form
+    {
+        private Tesseract _tesseract = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            _tesseract = new Tesseract();
+            _tesseract.Init("tessdata", "eng", OcrEngineMode.Default);
+        }
+
+        private void BrowseBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                LoadImageToPictureBox();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox1.Text))
+            {
+                MessageBox.Show("File does not exist");
+                return;
+            } 
+            LoadImageToPictureBox();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox1.Text))
+                return;
+            Bitmap fullImage = new Bitmap(textBox1.Text);
+
+            Bitmap fullImageGray = new Bitmap(fullImage.Width, fullImage.Height);
+
+            for (int i = 0; i < fullImage.Width; i++)
+            {
+                for (int x = 0; x < fullImage.Height; x++)
+                {
+                    Color oc = fullImage.GetPixel(i, x);
+                    int grayScale = (int)((oc.R * 0.3) + (oc.G * 0.59) + (oc.B * 0.11));
+                    Color nc = Color.FromArgb(oc.A, grayScale, grayScale, grayScale);
+                    fullImageGray.SetPixel(i, x, nc);
+                }
+            }
+
+            richTextBox1.Text = getImageText(fullImageGray);
+        }
+
+        private void LoadImageToPictureBox()
+        {
+            pictureBox1.Image = new Bitmap(textBox1.Text);
+        }
+
+        private string getImageText(Bitmap image)
+        {
+            List<string> possibleResults = new List<string>();
+            Image<Bgr, byte> emguFullImage = new Image<Bgr, byte>(image);
+
+            getPartialImageText(image, possibleResults, emguFullImage, 1);
+            getPartialImageText(image, possibleResults, emguFullImage, 2);
+            getPartialImageText(image, possibleResults, emguFullImage, 3);
+
+            return GetBestResult(possibleResults);
+        }
+
+        private void getPartialImageText(Bitmap image, List<string> possibleResults, Image<Bgr, byte> emguFullImage, int partOfImage)
+        {
+            int partialWidth = image.Width / partOfImage;
+            int partialHeight = image.Height / partOfImage;
+
+            for (int i = 0; i < partOfImage; i++)
+            {
+                int startX = i * partialWidth;
+
+                for (int j = 0; j < partOfImage; j++)
+                {
+                    int startY = j * partialHeight;
+                    //full row
+                    GetPartImageText(possibleResults, emguFullImage, image.Width, partialHeight, 0, startY);
+                    // slice
+                    GetPartImageText(possibleResults, emguFullImage, partialWidth, partialHeight, startX, startY);
+                }
+            }
+        }
+
+        private void GetPartImageText(List<string> possibleResults, Image<Bgr, byte> emguFullImage, int partialWidth, int partialHeight, int startX, int startY)
+        {
+            emguFullImage.ROI = new Rectangle(startX, startY, partialWidth, partialHeight);
+            _tesseract.SetImage(emguFullImage);
+            _tesseract.Recognize();
+            string imageText = _tesseract.GetUTF8Text();
+            if (string.IsNullOrEmpty(imageText))
+                return;
+            string[] lines = imageText.Split( new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+            {
+                string cleanLine = CleanLine(line);
+                if (!string.IsNullOrEmpty(cleanLine))
+                    possibleResults.Add(cleanLine);
+            }
+        }
+
+        private string CleanLine(string line)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in line)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    sb.Append(c);
+                }
+            }
+            string result = sb.ToString();
+            return (result.Length > 1)? result : string.Empty;
+        }
+
+        private string GetBestResult(List<string> possibleResults)
+        {
+            string longestString = string.Empty;
+            Dictionary<string, int> commonStrings = new Dictionary<string, int>();
+
+            foreach (var result in possibleResults)
+            {
+                if (result.Length > longestString.Length)
+                    longestString = result;
+                if (commonStrings.ContainsKey(result))
+                    commonStrings[result]++;
+                else
+                    commonStrings.Add(result, 1);
+            }
+
+            var mostCommon = commonStrings.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+            return (longestString.Contains(mostCommon)) ? longestString : mostCommon;
+        }
+    }
+}
